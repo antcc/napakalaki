e3b21e8479046493f9737bf87bda326ca860d55a
antcc <antoniocoin07@gmail.com>
2017-04-23 14:11:01 +0200

    P4S1: Crea nuevas clases

---
+++ Napakalaki/src/NapakalakiGame/Cultist.java
@@ -0,0 +1,29 @@
+/**
+ * Cultist.java
+ */
+
+package NapakalakiGame;
+
+/**
+ * Representa una carta de sectario.
+ */
+public class Cultist {
+    
+    private String name;
+    private int gainedLevels;
+    
+    public Cultist(String name, int gainedLevels) {
+        this.name = name;
+        this.gainedLevels = gainedLevels;
+    }
+    
+    public int getGainedLevels() {
+        return gainedLevels;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nombre: " + name + "\nNiveles ganados: " + Integer.toString(gainedLevels);
+    }
+    
+}
---
+++ Napakalaki/src/NapakalakiGame/CultistPlayer.java
@@ -0,0 +1,57 @@
+/**
+ * CultistPlayer.java
+ */
+
+package NapakalakiGame;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Representa un jugador sectario 
+ */
+public class CultistPlayer extends Player {
+    
+    private static int totalCultistPlayers = 0;
+    private Cultist myCultistCard;
+    
+    public CultistPlayer(Player p, Cultist c) {
+        super(p);
+        myCultistCard = c;
+        totalCultistPlayers++;
+    }
+    
+    @Override
+    protected int getCombatLevel() {
+        // super.getCombatLevel() + 0.7 * super.getCombatLevel() = 1.7 * super.getCombatLevel()
+        return (int) (1.7 * super.getCombatLevel()) + 
+                      myCultistCard.getGainedLevels() * totalCultistPlayers;
+    }
+    
+    @Override
+    protected int getOponentLevel(Monster m) {
+        return m.getCombatLevelAgainstCultistPlayer();
+    }
+    
+    @Override
+    protected boolean shouldConvert() {
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + "\nCarta de sectario:\n" + myCultistCard.toString();
+    }
+    
+    private boolean canYouGiveMeATreasure() {
+        return !getVisibleTreasures().isEmpty();
+    }
+    
+    private Treasure giveMeATreasure() {
+        return getVisibleTreasures().remove(new Random().nextInt(getVisibleTreasures().size()));
+    }
+    
+    public static int getTotalCultistPlayers() {
+        return totalCultistPlayers;
+    }
+}
